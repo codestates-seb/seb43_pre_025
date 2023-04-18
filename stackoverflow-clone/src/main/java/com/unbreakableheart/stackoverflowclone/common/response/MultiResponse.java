31eecd59c23a6f5a2d2cadf9ce020c917d49ce53
zqwz <zqwz@naver.com>
2023-04-18 11:25:26 +0900

    [BE] 공통 Response 양식 페이지네이션 적용

--- stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/response/MultiResponse.java
+++ stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/response/MultiResponse.java
@@ -1,13 +1,18 @@
 package com.unbreakableheart.stackoverflowclone.common.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Getter
 public class MultiResponse<T> {
     private PageInfo pageInfo;
     private List<T> data;
+
+    public MultiResponse(Page page, List<T> data) {
+        this.pageInfo = new PageInfo(page.getNumber() + 1, page.getSize(), page.getTotalElements(), page.getTotalPages());
+        this.data = data;
+    }
 }
