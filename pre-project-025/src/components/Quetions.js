24faf23eca5863e908db2a800bbca316480ef447
bread99 <puppy7962@gmail.com>
2023-04-18 14:23:35 +0900

    check

---
+++ pre-project-025/src/components/Quetions.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.h1`
+  font-size: 1.8rem;
+`;
+
+function Questions() {
+  return (
+    <main>
+      <StyledHeader>Top Questions</StyledHeader>
+    </main>
+  );
+}
+
+export default Questions;
