c4a1d9e50861c4156094453ab9d8a259b093377c
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-04-20 14:44:03 +0900

    fe : outlet 오류 해결

--- pre-project-025/src/App.js
+++ pre-project-025/src/App.js
@@ -1,29 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import GlobalStyle from "./assets/style/GlobalStyle";
-import QuestionList from "./components/QuestionList";
-
+import Login from './components/Login';
+import Signup from './components/SignUp';
 // import Questions from "./components/Questions";
-// import QuestionList from "./components/QuestionList";
+import QuestionList from "./components/QuestionList";
 // import { createGlobalStyle } from """;
 
-// const GlobalStyles = createGlobalStyle`
-//   body{
-//     width: 100%;
-//     background-color: white;
-//   }`;
+
+
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Header />
-      <Router>
         <Routes>
-          <Route path="/" element={<Main />}></Route>
-          {/* <Route index element={<QuestionList />}></Route> */}
+          <Route path="/" element={<Main />}>
+          <Route index element={<QuestionList />}></Route>
+          </Route>
         </Routes>
-      </Router>
+        <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        </Routes>
     </div>
 
   );
