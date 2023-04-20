990b9b110cf8757de32726fd5ee93d393240ae90
bread99 <puppy7962@gmail.com>
2023-04-20 17:05:35 +0900

    fe

--- pre-project-025/src/App.js
+++ pre-project-025/src/App.js
@@ -5,31 +5,19 @@ import GlobalStyle from "./assets/style/GlobalStyle";
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import QuestionList from "./pages/Questions/QuestionList";
-import AskQuestion from './pages/Questions/AskQuestion';
+import AskQuestion from "./pages/Questions/AskQuestion";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Header />
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Main />}>
           <Route index element={<QuestionList />}></Route>
         </Route>
       </Routes>
       <Routes>
-=======
-      {/* outlet router용 */}
-        <Routes>
-          <Route path="/" element={<Main />}>
-          <Route index element={<QuestionList />}></Route>
-          </Route>
-        </Routes>
-        {/* common router용 */}
-        <Routes>
-        <Route path="/ask" element={<AskQuestion />} />
->>>>>>> 84bcdda15ec79dc30c21a47b781b781f79394a86
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
