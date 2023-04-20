d2f0707895f9884d3b5a05464ca4b748f594a4fa
hong <forhreve24@gmail.com>
2023-04-20 13:54:39 +0900

    fe-login,signup

--- pre-project-025/src/App.js
+++ pre-project-025/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
+import Signup from './components/SignUp'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
--- pre-project-025/src/index.js
+++ pre-project-025/src/index.js
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // eslint-disable-next-line react/jsx-no-undef
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
