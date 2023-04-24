2c98ab20a34b0e8a8e9161560ce86f7c4b32a5a9
zqwz <zqwz@naver.com>
2023-04-24 11:57:53 +0900

    [BE] 소셜 타입별 유저 정보 저장 클래스 추가

---
+++ stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/oauth/userinfo/ProviderType.java
@@ -0,0 +1,16 @@
+package com.unbreakableheart.stackoverflowclone.common.security.oauth.userinfo;
+
+import lombok.Getter;
+
+@Getter
+public enum ProviderType {
+    GOOGLE("google"),
+    NAVER("naver"),
+    KAKAO("kakao");
+
+    private String provider;
+
+    ProviderType(String provider) {
+        this.provider = provider;
+    }
+}
