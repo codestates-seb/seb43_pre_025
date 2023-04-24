cf0a0a381161af24275b34be691d0521a3d98161
zqwz <zqwz@naver.com>
2023-04-24 12:01:57 +0900

    [BE] OAuth Config 설정

--- stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/config/SecurityConfig.java
+++ stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.unbreakableheart.stackoverflowclone.common.security.config;
 
 import com.unbreakableheart.stackoverflowclone.common.security.jwt.JwtAuthenticationFilter;
 import com.unbreakableheart.stackoverflowclone.common.security.jwt.JwtTokenProvider;
+import com.unbreakableheart.stackoverflowclone.common.security.oauth.handler.OAuth2LoginFailureHandler;
+import com.unbreakableheart.stackoverflowclone.common.security.oauth.handler.OAuth2LoginSuccessHandler;
+import com.unbreakableheart.stackoverflowclone.common.security.oauth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,26 +22,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final CorsConfig corsConfig;
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().disable()
+        httpSecurity
+                .httpBasic().disable()
+                .formLogin().disable()
                 .csrf().disable()
+                .headers().frameOptions().disable()
+
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
                 .and()
                 .authorizeRequests()
-                .antMatchers("/signup").permitAll()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .antMatchers("/api/login").permitAll()
                 .anyRequest().authenticated()
+
                 .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .oauth2Login()
+                .successHandler(oAuth2LoginSuccessHandler)
+                .failureHandler(oAuth2LoginFailureHandler)
+                .userInfoEndpoint()
+                .userService(oAuth2UserService);
+
+        httpSecurity.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilter(corsConfig.corsFilter());
         return httpSecurity.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
