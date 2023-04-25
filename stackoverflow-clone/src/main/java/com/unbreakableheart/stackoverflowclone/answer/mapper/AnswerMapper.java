90a3030225fb6371cf8c4b731e0c07621d4f186f
SOONUK-k <kswnuguri@gmail.com>
2023-04-25 22:22:28 +0900

    "맵퍼 경고 수정 커밋"

--- stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/answer/mapper/AnswerMapper.java
+++ stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/answer/mapper/AnswerMapper.java
@@ -4,10 +4,11 @@ import com.unbreakableheart.stackoverflowclone.answer.dto.AnswerDto;
 import com.unbreakableheart.stackoverflowclone.answer.entity.Answer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AnswerMapper {
 
     @Mapping(source = "userId", target = "user.id")
