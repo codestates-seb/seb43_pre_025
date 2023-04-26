c5cbf96bef54404a66e9d5265a87c1e4d7861145
zqwz <zqwz@naver.com>
2023-04-26 13:23:17 +0900

    [BE] Cors 설정 및 antMatchers 수정

--- stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/config/SecurityConfig.java
+++ stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/config/SecurityConfig.java
@@ -1,38 +1,57 @@
 package com.unbreakableheart.stackoverflowclone.common.security.config;
 
-import com.unbreakableheart.stackoverflowclone.common.security.jwt.JwtAuthenticationFilter;
+import com.unbreakableheart.stackoverflowclone.common.security.jwt.filter.JwtAuthenticationFilter;
 import com.unbreakableheart.stackoverflowclone.common.security.jwt.JwtTokenProvider;
+import com.unbreakableheart.stackoverflowclone.common.security.jwt.filter.JwtVerificationFilter;
+import com.unbreakableheart.stackoverflowclone.common.security.jwt.handler.UserAuthenticationFailureHandler;
+import com.unbreakableheart.stackoverflowclone.common.security.jwt.handler.UserAuthenticationSuccessHandler;
 import com.unbreakableheart.stackoverflowclone.common.security.oauth.handler.OAuth2LoginFailureHandler;
 import com.unbreakableheart.stackoverflowclone.common.security.oauth.handler.OAuth2LoginSuccessHandler;
 import com.unbreakableheart.stackoverflowclone.common.security.oauth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final CorsConfig corsConfig;
     private final CustomOAuth2UserService oAuth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        log.info("SecurityFilterChain 진입점");
         httpSecurity
                 .httpBasic().disable()
                 .formLogin().disable()
                 .csrf().disable()
+                .apply(new CustomFilterConfigurer())
+
+                .and()
+                .cors().configurationSource(corsConfigurationSource())
+
+                .and()
                 .headers().frameOptions().disable()
 
                 .and()
@@ -41,9 +60,13 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/signup").permitAll()
-                .antMatchers("/api/login").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.POST, "/api/questions/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/api/questions/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/questions/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/answers/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/api/answers/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/answers/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/**").permitAll()
 
                 .and()
                 .logout()
@@ -56,14 +79,46 @@ public class SecurityConfig {
                 .userInfoEndpoint()
                 .userService(oAuth2UserService);
 
-        httpSecurity.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                UsernamePasswordAuthenticationFilter.class);
-        httpSecurity.addFilter(corsConfig.corsFilter());
         return httpSecurity.build();
     }
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        log.info("CorsConfigurationSource 진입점");
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        log.info("CorsConfigurationSource 종료");
+        return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenProvider);
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new UserAuthenticationSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new UserAuthenticationFailureHandler());
+            jwtAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenProvider);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+        }
     }
 }
