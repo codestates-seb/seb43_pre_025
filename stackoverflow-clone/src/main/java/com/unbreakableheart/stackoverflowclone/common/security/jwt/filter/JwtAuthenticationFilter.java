9e3e27782d5f1c41f7534acbb2f8383317b0a64e
zqwz <zqwz@naver.com>
2023-04-27 12:15:17 +0900

    [BE] 로그인 response 추가

--- stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/jwt/filter/JwtAuthenticationFilter.java
+++ stackoverflow-clone/src/main/java/com/unbreakableheart/stackoverflowclone/common/security/jwt/filter/JwtAuthenticationFilter.java
@@ -1,12 +1,16 @@
 package com.unbreakableheart.stackoverflowclone.common.security.jwt.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.unbreakableheart.stackoverflowclone.common.security.jwt.JwtTokenProvider;
 import com.unbreakableheart.stackoverflowclone.user.dto.LoginDto;
+import com.unbreakableheart.stackoverflowclone.user.dto.UserDto;
 import com.unbreakableheart.stackoverflowclone.user.entity.User;
 import com.unbreakableheart.stackoverflowclone.user.entity.UserPrincipal;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,6 +21,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Slf4j
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -55,6 +60,27 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         response.setHeader("Authorization", "Bearer " + accessToken);
         response.setHeader("Refresh", refreshToken);
+
+        Gson gson = new Gson();
+        UserDto.Response userResponse = userToUserDtoResponse(user);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.CREATED.value());
+        response.getWriter().write(gson.toJson(userResponse, UserDto.Response.class));
+
         this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+    }
+
+    private UserDto.Response userToUserDtoResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        Long id = user.getId();
+        String email = user.getEmail();
+        String username = user.getUsername();
+        LocalDateTime createdAt = user.getCreatedAt();
+        LocalDateTime updatedAt = user.getUpdatedAt();
+
+        return new UserDto.Response( id, email, username, createdAt, updatedAt );
     }
 }
